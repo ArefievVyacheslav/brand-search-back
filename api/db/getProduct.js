09ff7a086e30c37118e88dbba8110aabf9c569cf
Arefiev Vyacheslav <arefievvyacheslavspb@gmail.com>
2022-09-11 11:48:01 +0300

    f

--- api/db/getProduct.js
+++ api/db/getProduct.js
@@ -6,7 +6,7 @@ module.exports = async function getProduct(id) {
     await client.connect()
     const db = await client.db('ss')
     const collection = await db.collection('all')
-    return id.includes('1111111') ? await collection.findOne({idd: id}) : await collection.findOne({id: id})
+    return id.includes('1111111') ? await collection.findOne({idd: +id}) : await collection.findOne({id: id})
   } catch (e) {
     console.log(e);
   }
