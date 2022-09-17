2e217c74d1fae2d5a731aba171b2adccc67ac503
Arefiev Vyacheslav <arefievvyacheslavspb@gmail.com>
2022-09-17 18:47:23 +0300

    f

--- api/index.js
+++ api/index.js
@@ -43,9 +43,9 @@ app.get('/api/products', async (req, res) => {
 app.get('/api/get-started', async (req, res) => {
   selects = await getSelects()
   await axios.get('https://api.sales-search.ru/api/main')
-  // await axios.get('http://localhost:3001/api/sitemap/123456789')         // перед пушем комменчу
-  // const { data } = await axios.get('http://localhost:3001/api/products') // перед пушем комменчу
-  const { data } = await axios.get('https://api.sales-search.ru/api/products')                   // локально комменчу
+  await axios.get('http://localhost:3001/api/sitemap/123456789')         // перед пушем комменчу
+  const { data } = await axios.get('http://localhost:3001/api/products') // перед пушем комменчу
+  // const { data } = await axios.get('https://api.sales-search.ru/api/products')                   // локально комменчу
   products = data
   res.send('OK!!!')
 })
