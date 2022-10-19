e480f717a784899b6eb078d50bf4457f8317159f
Arefiev Vyacheslav <arefievvyacheslavspb@gmail.com>
2022-10-19 20:47:57 +0300

    fp

--- api/db/getProducts.js
+++ api/db/getProducts.js
@@ -37,8 +37,7 @@ module.exports = async function getProducts(paramsObj) {
     const params = paramsArrKeys.reduce((ttlObj,keyParam) => {
 
       Object.keys(gender).forEach(keyGender => {
-        if (keyParam.includes(keyGender)) keyGender === '-child'
-          ? ttlObj['params.age'] = gender[keyGender] : ttlObj['params.gender'] = { $in: [gender[keyGender], 'unisex'] }
+        if (keyParam.includes(keyGender)) ttlObj.category = gender[keyGender]
       })
 
       Object.keys(category).forEach(keyCategory => {
